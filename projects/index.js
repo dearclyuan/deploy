d4f47fea305432dbfaa2cf1219fcc05925de6da5
dearclyuan <dearclyuan>
2017-11-29 12:21:32 +0800

    init deploy

--- projects/index.js
+++ projects/index.js
@@ -1,11 +1,11 @@
 const http = require('http')
 const shell = require('shelljs')
 const createHandler = require('github-webhook-handler')
-const handler = createHandler({ path: '/webhook', secret: 'surmon' })
+const handler = createHandler({ path: '/webhook', secret: 'vshop' })
 // 上面的 secret 保持和 GitHub 后台设置的一致
 
 const port = 9988
-const projects = ['shudong.wang','deploy']
+const projects = ['maxzh.itnote.cn', 'vshop', 'deploy', 'deploy-vshop']
 
 const projectHandler = (event, action) => {
 	const project = event.payload.repository.name
